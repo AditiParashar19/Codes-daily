39eb53bb966ba2db99379672c60876cbfeb4ba88
AditiParashar19 <aditiparashar22@gmail.com>
2026-03-22 11:50:22 +0530

    Add files via upload

---
+++ Streamlit/agecalculator.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import datetime
+
+st.title("Age Calculator")
+st.text("-------------Age Calculation by Date of Birth---------------")
+
+start_date = datetime.date(1900,1,1)
+end_date = datetime.date(2600,3,1)
+
+dob = st.date_input("Enter your date of birth",min_value=start_date,max_value=end_date)
+
+if(st.button("Calculate Age")):
+    if(dob):
+        today_date = datetime.date.today()
+        age = today_date.year - dob.year
+        month = today_date.month-dob.month
+        if(today_date.month < dob.month):
+            month = today_date.month
+        if(today_date.month == dob.month):
+            month =0
+        day =0
+        if((today_date.month,today_date.day)<(dob.month,dob.day)):
+            age-=1
+        st.success(f"You are {age} years old")
+    else:
+        st.write("Enter Date of Birth")
