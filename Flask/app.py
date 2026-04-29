3b39401cfb0130ecc30aaaa703740694e8c1707b
AditiParashar19 <aditiparashar22@gmail.com>
2026-04-29 13:58:25 +0530

    Initialize Flask app with basic routes

---
+++ Flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/contact')
+def  contact():
+    return render_template('contact.html')
