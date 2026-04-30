234fb5f1c75df43f7fa856a93865c9ddeeb9885d
AditiParashar19 <aditiparashar22@gmail.com>
2026-04-30 15:26:34 +0530

    Add payment and multilinestring routes, update home route
    Added new routes for payment and multilinestring, and updated home route to pass variables.

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask,render_template,jsonify   #Flask - class ,render_template - function
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',variable1="aditi",variable2=4245)
 
 @app.route('/menu')
 def menu():
@@ -13,3 +13,12 @@ def menu():
 @app.route('/contact')
 def  contact():
     return render_template('contact.html')
+
+@app.route('/payment')
+def payment():
+    return render_template('payment.html')
+
+@app.route('/multilinestring')
+def multilinestring():
+    return '''<h1>First Heading!!</h1>
+    <h2>This is second heading!!</h2>'''
